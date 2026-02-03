b53a4070d257b04d0ba565865c354073bc6fd5e1
GatehiFelix <felixnyoike894@gmail.com>
2026-02-03 12:09:26 +0300

    Added search functionality to the header search bar and the results in the home screen

--- client/src/screens/Home/index.jsx
+++ client/src/screens/Home/index.jsx
@@ -1,37 +1,53 @@
-import Loader from "@components/Loader";
-import Alert from "@components/Alert";
-import ProductCard from "@components/ProductCard";
+import { useNavigate, useParams } from 'react-router-dom';
+
+import Alert from '@components/Alert';
+import Loader from '@components/Loader';
+import Paginate from '@components/Paginate';
+import ProductCard from '@components/ProductCard';
+import { ArrowLeftIcon } from '@heroicons/react/24/outline';
 import { useGetProductsQuery } from '@slices/productApiSlice';
-import { useParams } from "react-router-dom";
-import Paginate from "@components/Paginate";
 
 const HomeScreen = () => {
+	const { pageNumber, keyword } = useParams();
+	const navigate = useNavigate();
 
-    const {pageNumber} = useParams();
-    const { data, isLoading, isError, error} = useGetProductsQuery({ pageNumber });
+	const { data, isLoading, isError, error } = useGetProductsQuery({
+		pageNumber,
+		keyword,
+	});
 
+	return (
+		<section className='bg-white'>
+			<div className='mx-auto max-w-7xl px-3 py-10 sm:px-6 lg:px-8'>
+				{isLoading ? (
+					<Loader />
+				) : isError ? (
+					<Alert type='error'>{error.data?.message || error?.error}</Alert>
+				) : (
+					<>
+						<div className='flex items-center gap-2'>
+							{keyword && (
+								<button onClick={() => navigate('/')} type='button'>
+									<span className='sr-only'>Go back</span>
+									<ArrowLeftIcon className='mr-1 h-5 w-5 text-slate-900' />
+								</button>
+							)}
+							<h1 className='text-2xl font-bold text-slate-900'>
+								{keyword ? `Search Results for ${keyword}` : 'Latest Products'}
+							</h1>
+						</div>
+						<div className='mt-6 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+							{data.products.map((product) => (
+								<ProductCard key={product._id} product={product} />
+							))}
+						</div>
+					</>
+				)}
 
-    return (
-        <section className="bg-white">
-            <div className="mx-auto max-w-7xl px-3 py-10 sm:px-6 lg:px-6">
-                <h1 className="text-2xl font-bold text-slate-950">Latest Products</h1>
-
-                {isLoading ? (
-                    <Loader />
-                ): isError ? (
-                    <Alert type="error">{error?.data?.message || error.error}</Alert>
-                ) : (
-                    <div className="mt-6 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                          {data?.products?.map((product) => (
-                              <ProductCard key={product._id} product={product} />
-                          ))}
-                    </div>
-                )}
-
-                <Paginate pages={data?.pages} page={data?.page} />
-            </div>
-        </section>
-    )
-}
+				<Paginate pages={data?.pages} page={data?.page} />
+			</div>
+		</section>
+	);
+};
 
 export default HomeScreen;
